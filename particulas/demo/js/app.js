300c00f87a8167103e3230e164d8257abdc86403
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-07-17 20:41:23 -0300

    Update app.js

--- particulas/demo/js/app.js
+++ particulas/demo/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 314,
+        "value": 800,
         "density": {
           "enable": true,
           "value_area": 800
